bdb9ed144a3246f49ec51440925d9029bc78bb23
arthur tutu <canamopaaba@gmail.com>
2022-07-23 15:21:14 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slide-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
